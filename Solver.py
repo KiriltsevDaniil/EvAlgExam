840b54a46e9ffd9be7c6dd57e7cd2b25716679ec
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2022-05-04 20:10:36 +0300

    Rewrote Data_Gen as class

--- Solver.py
+++ Solver.py
@@ -1,81 +1,87 @@
 import numpy as np
 import math as m
-import DataGen as dg
+# import DataGen as dg
+from Generator import Generator as gen
+
 
 class SSWM:
-	'''Using SSWM in case of our problem'''
-	def __init__(self, W_matrix, B_matrix, C_vector, mutate="flipper", \
-		T_stop=2_000, beta=0.99, C_parm=2, lambda_parm=2, N=3,h=1, p_mut=0.5):
-		
-		assert C_parm > 1, "C_parm has to be more than 1"
-		assert lambda_parm > 0, "lambda_parm has to be more than 0"
+    '''Using SSWM in case of our problem'''
 
+    def __init__(self, K, M, N, mutate="flipper", \
+                 T_stop=2_000, beta=0.99, C_parm=2, lambda_parm=2, h=1, p_mut=0.5):
 
-		self.N = N
-		self.h = h
-		self.T_stop = T_stop
-		self.p_mut = p_mut
+        assert C_parm > 1, "C_parm has to be more than 1"
+        assert lambda_parm > 0, "lambda_parm has to be more than 0"
 
-		self.W_m = W_matrix
-		self.B_m = B_matrix
-		self.C_v = C_vector
+        self.generator = gen(K, M, N)
 
-		self.C_parm = C_parm
-		self.lambda_parm = lambda_parm
-		self.beta = beta
+        self.h = h
+        self.T_stop = T_stop
+        self.p_mut = p_mut
 
-		if mutate == "flipper":
-			self.mutate = self.flipper
+        self.W_m = self.generator.W_matrix()
+        # print(self.W_m)
+        self.B_m = self.generator.B_matrix()
+        # print(self.B_m)
+        self.C_v = self.generator.C_vector()
+        # print(self.C_v)
 
-		self.F_max = self.get_F_max()
-		self.fittest = None
+        self.C_parm = C_parm
+        self.lambda_parm = lambda_parm
+        self.beta = beta
 
-		self.generations = []
-		self.fitnesses = []
+        if mutate == "flipper":
+            self.mutate = self.flipper
 
-	def W(self, f_v):
-		return np.dot((self.B_m @ f_v.T),f_v)*0.5 + np.dot(self.C_v, f_v)
+        self.F_max = self.get_F_max()
+        self.fittest = None
 
-	def fitness(self, f_v):
-		return self.C_parm * m.exp(self.lambda_parm * self.W(f_v))
+        self.generations = []
+        self.fitnesses = []
 
-	def get_F_max(self):
-		inv_B_m = np.linalg.inv(self.B_m)
-		W_max = -0.5 * np.dot(inv_B_m @ self.C_v.T,self.C_v)
-		return self.C_parm * m.exp(W_max)
+    def W(self, f_v):
+        return np.dot((self.B_m @ f_v.T), f_v) * 0.5 + np.dot(self.C_v, f_v)
 
-	def flipper(self, genotype):
-		for i in range(len(genotype)):
-			if np.random.binomial(1, self.p_mut):
-				genotype[i] = 1 - genotype[i]
-		return genotype
+    def fitness(self, f_v):
+        return self.C_parm * m.exp(self.lambda_parm * self.W(f_v))
 
-	def solver(self, silent=False):
-		T = 0
-		self.S_v = dg.S_vector_gen(N=self.N)
-		f_v_gen = dg.F_vector_gen(self.S_v, self.W_m)
-		gen_fitness = self.fitness(f_v_gen)
-		print(f"Start_fitness: {gen_fitness}", f"Max_fitness: {self.F_max*self.beta}")
-		while T < self.T_stop and gen_fitness < self.F_max*self.beta:
-			
-			f_v_gen = dg.F_vector_gen(self.S_v, self.W_m)
-			gen_fitness = self.fitness(f_v_gen)
+    def get_F_max(self):
+        inv_B_m = np.linalg.inv(self.B_m)
+        W_max = -0.5 * np.dot(inv_B_m @ self.C_v.T, self.C_v)
+        return self.C_parm * m.exp(W_max)
 
-			next_gen = self.mutate(self.S_v.copy())
-			f_v_next_gen = dg.F_vector_gen(next_gen, self.W_m)
-			next_gen_fitness = self.fitness(f_v_next_gen)
+    def flipper(self, genotype):
+        for i in range(len(genotype)):
+            if np.random.binomial(1, self.p_mut):
+                genotype[i] = 1 - genotype[i]
+        return genotype
 
-			F = next_gen_fitness - gen_fitness
-			if F > 0:
-				self.S_v = next_gen
-			
-			if not silent:
-				print(f"Generation: {T}, Fitness: {gen_fitness}, diff: {F}, Gens:{self.S_v}/{next_gen}")
-				self.generations.append(T)
-				self.fitnesses.append(gen_fitness)
-			T += 1
-		self.fittest = self.S_v
+    def solver(self, silent=False):
+        T = 0
+        # self.S_v = dg.S_vector_gen(N=self.N)
+        # f_v_gen = dg.F_vector_gen(self.S_v, self.W_m)
+        self.S_v = self.generator.S_vector()
+        f_v_gen = self.generator.F_vector(self.S_v, self.W_m)
+        gen_fitness = self.fitness(f_v_gen)
+        print(f"Start_fitness: {gen_fitness}", f"Max_fitness: {self.F_max * self.beta}")
+        while T < self.T_stop and gen_fitness < self.F_max * self.beta:
 
+            # f_v_gen = dg.F_vector_gen(self.S_v, self.W_m)
+            f_v_gen = self.generator.F_vector(self.S_v, self.W_m)
+            gen_fitness = self.fitness(f_v_gen)
 
-if __name__ == "__main__":
-	pass
+            next_gen = self.mutate(self.S_v.copy())
+            # f_v_next_gen = dg.F_vector_gen(next_gen, self.W_m)
+            f_v_next_gen = self.generator.F_vector(next_gen, self.W_m)
+            next_gen_fitness = self.fitness(f_v_next_gen)
+
+            F = next_gen_fitness - gen_fitness
+            if F > 0:
+                self.S_v = next_gen
+
+            if not silent:
+                print(f"Generation: {T}, Fitness: {gen_fitness}, diff: {F}, Gens:{self.S_v}/{next_gen}")
+                self.generations.append(T)
+                self.fitnesses.append(gen_fitness)
+            T += 1
+        self.fittest = self.S_v
--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import Solver as slvr
-import DataGen as dg
+# import DataGen as dg
 import numpy as np
 
 from View import View
+from Generator import Generator
 
-r_seed = np.random.randint(10000)
-np.random.seed(r_seed)
+# r_seed = np.random.randint(10000)
+# np.random.seed(r_seed)
+r_seed = 4783
 
 
 K=2
 M=5
 N=5
-W_m = dg.W_matrix_gen(K=K, M=M)
-B_m = dg.B_matrix_gen(M=M)
-C_v = dg.C_vector_gen(M=M)
 
-sswm = slvr.SSWM(W_m, B_m, C_v, N=N)
+sswm = slvr.SSWM(K=K, M=M, N=N)
 sswm.solver()
 print("\n", sswm.fittest)
 
